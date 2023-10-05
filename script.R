f840879a26bba029d9a4e93d899070b918ef7d56
Fabian Oschwald <fatechde@gmail.com>
2023-10-05 17:13:58 +0200

    solved problem 2

---
+++ script.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+
+# Problem 2
+
+galaxy_data <- read_lines("data.txt") # read in data
+
+# calculate index for separator line
+seperator_index <-
+  galaxy_data %>% 
+  startsWith("-") %>% 
+  which()
+
+# omit unwanted lines
+galaxy_data <- galaxy_data[-c((1:(seperator_index-2)), seperator_index)]
+
+# write new "clean" file
+file <- file("output.txt")
+writeLines(galaxy_data, file)
+close(file)
+
+# read in new file using column names overwriting initial data
+galaxy_data <- read_delim("output.txt", delim = "|")
+
+
+
+
